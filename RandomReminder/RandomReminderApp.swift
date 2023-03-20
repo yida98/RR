9f4978fc2ad14b5b7f8a81e2f92aaadab7a117d2
Yida Zhang <carriedada@gmail.com>
2023-03-20 16:15:36 -0700

    Initial Commit

---
+++ RandomReminder/RandomReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomReminderApp.swift
+//  RandomReminder
+//
+//  Created by Yida Zhang on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
