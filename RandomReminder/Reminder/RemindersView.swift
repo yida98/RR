ba1393ed81099533a3ae1e410e64ca7fc4f32eaa
Yida Zhang <carriedada@gmail.com>
2023-03-22 19:39:11 -0700

    rr://1c365bcf9ac64d6581b69d3f92157f96 Reminder expansion

--- RandomReminder/Reminder/RemindersView.swift
+++ RandomReminder/Reminder/RemindersView.swift
@@ -10,19 +10,23 @@ import SwiftUI
 struct RemindersView: View {
     @Binding var isOpen: Bool
     var body: some View {
-        GeometryReader { proxy in
-            ScrollView(showsIndicators: false) {
-                VStack {
-                    ReminderCell()
-                }
+        ZStack {
+            if isOpen {
+                ReminderCell()
+                    .fixedSize(horizontal: false, vertical: true)
+            } else {
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        ForEach(0..<5) { ind in
+                            ReminderCell()
+                        }
+                    }
+                }.transition(.asymmetric(insertion: .push(from: .top), removal: .push(from: .bottom)))
             }
-            .padding(40)
-            .background(
-                DynamicToolsBackdropShape(isOpen: $isOpen, proxy: proxy)
-                    .fill(Color.background)
-            )
-            .clipShape(DynamicToolsBackdropShape(isOpen: $isOpen, proxy: proxy))
-            .animation(.linear, value: isOpen)
         }
+        .padding(40)
+        .background(DynamicToolsBackdrop(isOpen: $isOpen))
+        .mask(DynamicToolsBackdrop(isOpen: $isOpen))
+        .animation(.spring(), value: isOpen)
     }
 }
--- RandomReminder/Toolbar/DynamicToolsBackdrop.swift
+++ RandomReminder/Toolbar/DynamicToolsBackdrop.swift
@@ -11,30 +11,35 @@ struct DynamicToolsBackdrop: View {
     @Binding var isOpen: Bool
     var body: some View {
         GeometryReader { proxy in
-            DynamicToolsBackdropShape(isOpen: $isOpen, proxy: proxy)
+            DynamicToolsBackdropShape(isOpen: $isOpen, frameWidth: proxy.frame(in: .global).width, frameHeight: proxy.frame(in: .global).height)
                 .fill(Color.background)
-                .animation(.linear, value: isOpen)
+                .animation(.spring(), value: isOpen)
         }
     }
 }
 
 struct DynamicToolsBackdropShape: Shape {
     @Binding var isOpen: Bool
+    private var frameWidth: CGFloat
+    private var frameHeight: CGFloat
     private var divotY: CGFloat
     private var radius: CGFloat
-    
-    init(isOpen: Binding<Bool>, proxy: GeometryProxy) {
+
+    init(isOpen: Binding<Bool>, frameWidth: CGFloat, frameHeight: CGFloat) {
         self._isOpen = isOpen
-        self.radius = proxy.size.width / 10
-        self.divotY = isOpen.wrappedValue ? proxy.size.height + radius : proxy.size.height - radius
+        self.frameWidth = frameWidth
+        self.frameHeight = frameHeight
+        self.radius = frameWidth / 10
+        self.divotY = isOpen.wrappedValue ? frameHeight + radius : frameHeight - radius
     }
     
-    var animatableData: CGFloat {
+    var animatableData: AnimatablePair<CGFloat, CGFloat> {
         get {
-            divotY
+            AnimatableData(divotY, frameHeight)
         }
         set {
-            divotY = newValue
+            divotY = newValue.first
+            frameHeight = newValue.second
         }
     }
     
@@ -54,7 +59,7 @@ struct DynamicToolsBackdropShape: Shape {
                     clockwise: false)
         
         /// Right line, no corner
-        let bottomControlY = rect.maxY - radius
+        let bottomControlY = frameHeight - radius
         path.addLine(to: CGPoint(x: rect.maxX, y: bottomControlY))
         path.addArc(center: CGPoint(x: rightControlX, y: bottomControlY),
                     radius: radius,
@@ -64,16 +69,16 @@ struct DynamicToolsBackdropShape: Shape {
         
         /// Bottom line
         let leftControlX = radius
-        path.addLine(to: CGPoint(x: rect.midX + (2 * radius), y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX + (2 * radius), y: frameHeight))
         
         path.addCurve(to: CGPoint(x: rect.midX, y: divotY),
-                      control1: CGPoint(x: rect.midX + radius, y: rect.maxY),
+                      control1: CGPoint(x: rect.midX + radius, y: frameHeight),
                       control2: CGPoint(x: rect.midX + radius, y: divotY))
-        path.addCurve(to: CGPoint(x: rect.midX - (2 * radius), y: rect.maxY),
+        path.addCurve(to: CGPoint(x: rect.midX - (2 * radius), y: frameHeight),
                       control1: CGPoint(x: rect.midX - radius, y: divotY),
-                      control2: CGPoint(x: rect.midX - radius, y: rect.maxY))
+                      control2: CGPoint(x: rect.midX - radius, y: frameHeight))
         
-        path.addLine(to: CGPoint(x: leftControlX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: leftControlX, y: frameHeight))
         path.addArc(center: CGPoint(x: leftControlX, y: bottomControlY),
                     radius: radius,
                     startAngle: Angle.radians(Double.pi / 2),
